75877be7a02bf9556d21a9429df31e443475c41a
Shahab <hajinasiri@gmail.com>
2018-08-03 19:19:16 -0700

    separating functions from the main file

--- convertor.js
+++ convertor.js
@@ -1,115 +1,17 @@
-// var fs = require("fs");
-// var DOMParser = require('xmldom').DOMParser;
-// var _ = require('lodash');
 
+// //to download a file to client's computer
+// function download(filename, text) {
+//   var element = document.createElement('a');
+//   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+//   element.setAttribute('download', filename);
 
+//   element.style.display = 'none';
+//   document.body.appendChild(element);
 
-// const filename = "../../Downloads/Archive/workflow5.scriv/workflow5.scrivx";
-// var text= fs.readFileSync(filename, "utf8");
+//   element.click();
 
-
-// var parser = new DOMParser()
-// var el = parser.parseFromString(text, "text/xml");
-// const Binder = el.getElementsByTagName("Binder");
-// // console.log(el.documentElement == "parsererror" ? "error while parsing" : el.documentElement);
-
-
-
-
-function flatten(object) {
-  var check = _.isPlainObject(object) && _.size(object) === 1;
-  return check ? flatten(_.values(object)[0]) : object;
-}
-
-//to parse xml to an object
-function parse(xml) {
-  var data = {};
-
-  var isText = xml.nodeType === 3,
-      isElement = xml.nodeType === 1,
-      body = xml.textContent && xml.textContent.trim(),
-      hasChildren = xml.children && xml.children.length,
-      hasAttributes = xml.attributes && xml.attributes.length;
-
-  // if it's text just return it
-  if (isText) { return xml.nodeValue.trim(); }
-
-  // if it doesn't have any children or attributes, just return the contents
-  if (!hasChildren && !hasAttributes) { return body; }
-
-  // if it doesn't have children but _does_ have body content, we'll use that
-  if (!hasChildren && body.length) { data.text = body; }
-
-  // if it's an element with attributes, add them to data.attributes
-  if (isElement && hasAttributes) {
-    data.attributes = _.reduce(xml.attributes, function(obj, name, id) {
-      var attr = xml.attributes.item(id);
-      obj[attr.name] = attr.value;
-      return obj;
-    }, {});
-  }
-
-  // recursively call #parse over children, adding results to data
-  _.each(xml.children, function(child) {
-    var name = child.nodeName;
-
-    // if we've not come across a child with this nodeType, add it as an object
-    // and return here
-    if (!_.has(data, name)) {
-      data[name] = parse(child);
-      return;
-    }
-
-    // if we've encountered a second instance of the same nodeType, make our
-    // representation of it an array
-    if (!_.isArray(data[name])) { data[name] = [data[name]]; }
-
-    // and finally, append the new child
-    data[name].push(parse(child));
-  });
-
-  // if we can, let's fold some attributes into the body
-  _.each(data.attributes, function(value, key) {
-    if (data[key] != null) { return; }
-    data[key] = value;
-    delete data.attributes[key];
-  });
-
-  // if data.attributes is now empty, get rid of it
-  if (_.isEmpty(data.attributes)) { delete data.attributes; }
-
-  // simplify to reduce number of final leaf nodes and return
-  return flatten(data);
-}
-
-//to download a file to client's computer
-function download(filename, text) {
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
-
-  element.style.display = 'none';
-  document.body.appendChild(element);
-
-  element.click();
-
-  document.body.removeChild(element);
-}
-
-
-
-
-
-//to find the .scrivx file and return the corresponding array element
-function findFile (files,key,str) {
-  var result='';
-  for (i = 0; i < files.length; i++ ){
-      if (files[i][key].indexOf(str) > -1){
-        result = (files[i]);
-      }
-  }
-  return result;
-}
+//   document.body.removeChild(element);
+// }
 
 
 
@@ -134,19 +36,24 @@ function readSingleFile(f,cb) {
       cb(XML);
     };
     r.readAsText(f);
-
   } else {
     alert("Failed to load file");
   }
-
-
 }
 
 
+
 function main(evt) {
-  const f =findFile(evt.target.files,'name','.scrivx');
+  const files = evt.target.files;
+  const f =findFile(files,'name','.scrivx');
   const data = readSingleFile(f,function(XML){
-    console.log(XML.Binder);
+    const UUID = XML.Binder[0].Children[0].UUID;
+    const textPath = findFile(files,'webkitRelativePath',UUID);
+    // console.log(textPath);
+    // console.log(files);
+    myAsyncFunction().then(val => {
+      console.log(val);
+    })
   });
 
 }
@@ -154,7 +61,4 @@ function main(evt) {
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('fileinput').addEventListener('change', main, false);
 });
-
-
-
 
