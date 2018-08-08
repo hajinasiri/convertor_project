263ff55458245fc061b385c2709d2cdd8b2056ec
Shahab <hajinasiri@gmail.com>
2018-08-07 18:56:14 -0700

    row number calculation was added

--- convertor.js
+++ convertor.js
@@ -10,7 +10,7 @@ function addElement(object,files,excel,index,column){
   return rowAdditionNumber;
 }
 
-function singleElement(element){
+function singleElement(element,index,excel,row){
   var counter =[0];
   var finish = false;
   var c =0;
@@ -19,7 +19,7 @@ function singleElement(element){
 
   while  (finish === false && c<100 ) {
     validation = true;
-    console.log(counter);
+    // console.log(counter);
 
     for(i=0; i<counter.length; i++){
       if(target.Children[counter[i]]){
@@ -31,7 +31,9 @@ function singleElement(element){
 
 
     if(validation){
-       console.log(target);
+       console.log(target.Title);
+       console.log(row);
+       row += 1;
       if(target.Children){
         counter.push(0);
       }else{
@@ -50,15 +52,21 @@ function singleElement(element){
     }
 
   }
-  console.log(c);
+  return row;
 }
 
 
 function createExcel(files,XML){
   var Binder = XML.Binder;
-  Binder.forEach(function(element){
+  var excel = $JExcel.new();
+  excel.set({row:1,column:1,value:'Title'});
+  var row = 2;
+  Binder.forEach(function(element,index){
+    console.log(element.Title);
+    console.log(row);
+    row += 1;
     if(element.Children){
-      singleElement(element);
+      singleElement(element,index,excel,row);
     }
 
   })
@@ -108,39 +116,6 @@ function createExcel(files,XML){
 //   setTimeout(function(){excel.generate("SampleData.xlsx"); }, 600);
 // }
 
-// function createExcel(files,XML){
-//   const Binder = XML.Binder;
-//   var excel = $JExcel.new();
-//   excel.set({row:1,column:1,value:'Title'})
-//   var childRowIndex = 2;
-//   Binder.forEach(function(child){
-//     excel.set({row:childRowIndex ,column:1,value:child.Title});
-//     if(child.Children && Array.isArray(child.Children)){//checking if the child has children and children is an array
-//       grandChildRowIndex = childRowIndex  + 1;//setting the starting row for the children
-//       var addition = 0;
-//        child.Children.forEach(function(grandChild){
-//         addition = addElement(grandChild,files,excel,grandChildRowIndex,2);
-//         grandChildRowIndex += addition;
-//         childRowIndex  += addition;
-//         if(grandChild.Children && Array.isArray(grandChild.Children)){
-//           // const forthGenerationRowIndex = grandChildRowIndex + 1;
-//           // grandChild.Children.forEach(function(forthGeneration){
-//           //   excel.set({row:forthGenerationRowIndex,column:3,value:forthGeneration.Title});
-
-//           // })
-
-//         }
-//       });
-
-//       childRowIndex  += 1+child.Children.length;
-//     }else{
-//       childRowIndex += 1;
-//     }
-
-//   });
-//   setTimeout(function(){excel.generate("SampleData.xlsx"); }, 600);
-// }
-
 
 
 function main(evt) {
@@ -159,4 +134,3 @@ function main(evt) {
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('fileinput').addEventListener('change', main, false);
 });
-
