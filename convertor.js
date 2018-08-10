037faba2b8c3922342aaf70180263413a0ead243
Shahab <hajinasiri@gmail.com>
2018-08-09 18:01:41 -0700

    parent column adding code was added

--- convertor.js
+++ convertor.js
@@ -1,11 +1,17 @@
-function addElement(target,files,counter,row,excel,uno){
+function addElement(target,files,counter,row,excel,uno,parent){
   // getText(files,excel,target,index,column+1);//if the grandChild has UUID this function will add the text to the excel file
   uno = uno.map(a => a.toLowerCase());//Makes all uno titles lowercase to be able to search them
   const outline = counter.map(a => a+1).map(String).reduce((a, b) => a + '.'+ b); //calculates outline number
   excel.set({row:row,column:uno.indexOf('outlinenumber')+ 4,value:outline});
   excel.set({row:row,column:3,value:target.Title});//sets the title column in excel
   excel.set({row:row,column:uno.indexOf('label')+ 4,value:target.Title});//sets the label column in excel
+  if(counter[0] === -1){//sets the outlinelevel column
+    excel.set({row:row,column:uno.indexOf('outlinelevel')+ 4,value:0});//sets 0 for map
+  }else{
+    excel.set({row:row,column:uno.indexOf('outlinelevel')+ 4,value:counter.length});//calculates and sets outlinelevel for things other than map
+  }
   excel.set({row:row,column:uno.indexOf('id')+ 4,value:target.Title.replace(/ /g,'')});//sets the id column in excel
+  excel.set({row:row,column:uno.indexOf('parent')+ 4,value:parent});//sets the parent column in excel
   var CustomMetaData = target.MetaData.CustomMetaData;
   if(!Array.isArray(CustomMetaData)){
     CustomMetaData = [CustomMetaData];
@@ -32,6 +38,7 @@ function singleElement(element,index,excel,row,files,uno){
   while  (finish === false && c<100 ) {
     validation = true;
     // console.log(counter);
+    var parent = 'Map';
 
     for(i=0; i<counter.length; i++){
       if(target.Children[counter[i]]){
@@ -39,11 +46,15 @@ function singleElement(element,index,excel,row,files,uno){
       }else{
         validation = false;
       }
+      if(i === (counter.length - 2)){
+        parent = target.Title;
+      }
     }
 
 
     if(validation){
-       addElement(target,files,counter,row,excel,uno);
+      addElement(target,files,counter,row,excel,uno,parent);
+
        row += 1;
       if(target.Children){
         counter.push(0);
@@ -72,7 +83,8 @@ function initialize(excel){
   const uno = ["id", "label", "outlineNumber", "outlineLevel", "parent", "classes", "unoFrom", "unoTo", "param1", "param2",
   "param3", "param4", "shortDescription", "longDescription", "hoverAction", "hoverFunction", "clickAction", "clickFunction",
   "onDoubleClick", "url", "urlText", "tooltip", "infoPane", "onURL", "offURL", "openURL", "closeURL", "onFunction", "offFunction",
-  "openFunction", "closeFunction", "ttStyle", "render", "symbol", "location", "xpos","panzoom", "ypos", "xsize", "ysize", "xoffset", "yoffset","slideurl",'subtitle'];
+  "openFunction", "closeFunction", "ttStyle", "render", "symbol", "location", "xpos","panzoom", "ypos", "xsize", "ysize", "xoffset",
+  "yoffset","slideurl",'subtitle'];
   uno.forEach(function(element,index){
     excel.set({row:1,column:4+index,value:element});
   });
@@ -87,7 +99,7 @@ function createExcel(files,XML){
   const uno = initialize(excel);
   var row = 2;
   Binder.forEach(function(element,index){
-    addElement(element,files,[-1],row,excel,uno);
+    addElement(element,files,[-1],row,excel,uno,'Binder');
     // excel.set({row:row,column:3,value:element.Title});
     console.log(row);
     row += 1;
@@ -111,9 +123,7 @@ function main(evt) {
     const XML = parse(xml);
     createExcel(files,XML);
     console.log(XML);
-
   });
-
 }
 
 document.addEventListener('DOMContentLoaded', function () {
