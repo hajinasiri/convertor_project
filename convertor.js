331116225f76a03dacbcacc8bbf31baa2e624d4f
Shahab <hajinasiri@gmail.com>
2018-08-11 21:21:05 -0700

    some comments were added

--- convertor.js
+++ convertor.js
@@ -45,14 +45,6 @@ function propagate(XML,excel,row, target,parent,uno,counter){
     parentMetaData = [parentMetaData];
   }
 
-
-
-   if(target.Title === 'Map'){
-    console.log(target);
-    console.log(CustomMetaData);
-  }
-
-
   uno.forEach(function(element,index){//goes through all the MetaData and checks if the child has that value or the parent and puts that vlue in excel file
      var found = false;
     CustomMetaData.forEach(function(childData){//Checks if the child has a value for it
@@ -95,21 +87,22 @@ function propagate(XML,excel,row, target,parent,uno,counter){
 
 
 function singleElement(XML,element,index,excel,row,files,uno){
-  var counter =[0];
-  var finish = false;
-  var c =0;
+  var counter =[0];//counter is the address to the current target that the code looks at. The code updates counter and then uses it to get the child
+  //each element position in the array reperesent the number of generation of the target, and the value reperesents the number of the child in its generation.
+  var finish = false; //finish variable changes to true when all the childs have been looked at and their data has been extracted. When finish is true the loop stops
+  var validation;
+  var target = element; //target is the current child
+  var parent = element; //the parent of the current child is stored in parent. This varible will be used to get the MetaData and pass it to the target if the target does not have the value for MetaData
 
-  var target = element;
-  var parent = element;
-
-  while  (finish === false && c<100 ) {
+  while  (finish === false ) { //This loop starts from Map. If map has a child the loop updates variable counter to the address of that child. It goes on until the loop reaches the last genereation.
+    //Then goes one generation back and checks if there is any other child in that generation. If yes, updates the counter to the new address. If no, goes one more generation back.
+    //The loop continues searching like this until it goes through all the generation and children. Every child the loop goes through, uses addElement function to exctract the data from the child and add them to the excel file
     validation = true;
 
-
-    for(i=0; i<counter.length; i++){
-      if(target.Children[counter[i]]){
+    for(i=0; i<counter.length; i++){ //this loop uses counter ass the address to get to the child and store it in the target variable
+      if(target.Children[counter[i]]){//if there is a child in that address, sets the target
         target =target.Children[counter[i]];
-      }else{
+      }else{ // if there is not a child in that address sets validation false
         validation = false;
       }
       if(i === (counter.length - 2)){
@@ -118,24 +111,22 @@ function singleElement(XML,element,index,excel,row,files,uno){
     }
 
 
-    if(validation){
+    if(validation){//If the validation is true and there is a child in the address, addElement is used to add the data to the excel file
       addElement(XML,target,files,counter,row,excel,uno,parent);
-
-       row += 1;
-      if(target.Children){
+       row += 1; //goes to the next row in excel
+      if(target.Children){ //if the target has children sets the counter to the first of them
         counter.push(0);
-      }else{
+      }else{//if the target does not have children, sets counter to the next sibling of the target
         counter[counter.length-1] +=1
       }
 
-    }else{
+    }else{ //if there is not a child at the addressed obtained from the counter, sets counter to one generation back and the next sibling in that generation
       counter.splice(-1,1);
       counter[counter.length-1] +=1
     }
-    target = element;
-    c += 1;
+    target = element;//resets target to the first generation ancestor to make it ready to build the next address from
 
-    if(counter[0] == undefined){ // to stop the while loop when it has gone through all elements
+    if(counter[0] == undefined){ // when the loop goes through all the children and then comes back to the first generation and there is no more sibling there, it goes one generation back and first element in counter becomes undefined. That's how the loop realizes it should end.
       finish = true;
     }
 
@@ -163,36 +154,34 @@ function initialize(excel){ //initializes the MetaData columns inside excel file
 function createExcel(files,XML){//fetches data from XML, Uses addElement function to add the data to excel file. addElement function itself
   //uses propagate function to make child inherit MetaData from their parent
 
-  var Binder = [XML.Binder[0]];
-  var excel = $JExcel.new();
-  const uno = initialize(excel);
+  var Binder = [XML.Binder[0]]; //puts Map in Binder varialble
+  var excel = $JExcel.new(); //initiates excel file
+  const uno = initialize(excel);//uses initialize function to add all uno elements to the excel file. in rturn gets uno array and puts it in uno variable
   var row = 2;
-  Binder.forEach(function(element,index){
-    addElement(XML,element,files,[-1],row,excel,uno,XML.Binder);
-    // excel.set({row:row,column:3,value:element.Title});
+  Binder.forEach(function(element,index){ //this forEach is here to go through all the elements in Binder if needed. But currently there is only map in Binder variable
+    addElement(XML,element,files,[-1],row,excel,uno,{Title:'Binder'}); //This line adds all the data of Map to excel file
+    row += 1; //Then sets row number to the next row number
 
-    row += 1;
-
-    if(element.Children){
+    if(element.Children){ //if there is children in Map, uses singleElement function to go through all the children inside map and gets the last row number from that function
       row = singleElement(XML,element,index,excel,row,files,uno);
     }
 
   })
-  excel.generate('converted.xlsx');
+  excel.generate('converted.xlsx'); //generates the excel file
 
 }
 
 
 
 
-function main(evt) {
-  const files = evt.target.files;
-  const f =findFile(files,'name','.scrivx','');
+function main(evt) { //This is the main function. Gets triggered when the button on the browser is clicked.
+  const files = evt.target.files; //puts the read files in variable files
+  const f =findFile(files,'name','.scrivx',''); //finds the scrivx file and puts it in f
 
-  readSingleFile(f,function(text1){
-    const xml = new DOMParser().parseFromString(text1, "text/xml");
-    const XML = parse(xml);
-    createExcel(files,XML);
+  readSingleFile(f,function(text1){//reads the scrivx file and as call back puts it in xml, parses it and creates the excel file from it
+    const xml = new DOMParser().parseFromString(text1, "text/xml"); //Parses the text into a DOM and puts it in xml variable
+    const XML = parse(xml);//uses parse function to create an object from the DOM and puts it in XML variable
+    createExcel(files,XML);//uses createExcel function to create the excel file from XML. files variable is passed to the function to read more files from it if needed
     console.log(XML);
   });
 
