6947085bc39cf733df32788dd16b5505d66f0e20
Shahab <hajinasiri@gmail.com>
2018-09-12 15:59:10 -0700

    the main funcation was added and was executed inside ncp as callback

--- excelToScriv.js
+++ excelToScriv.js
@@ -8,38 +8,34 @@ var f = "/Users/shahab/lighthouse/scriv/excelToScriv/render0.3.xlsx";
 const scriv = f.substr(0,f.indexOf('xlsx')) + 'scriv'; //builds the destination path for scriv file
 const fileName = scriv.substr(scriv.lastIndexOf('/'), scriv.length) + 'x';
 const scrivx =scriv +fileName; //builds the destination path for scrivx file
-
 ncp.limit = 16;//part of the module
 ncp('./template.scriv', scriv, function (err) { //copies the template scriv file to the destination of scriv with the final name
  if (err) {
    return console.error(err);
+ }else{
+
+  main();
  }
  console.log('done!');
 });
 
-fs.rename(scriv + 'template.scrivx', scrivx, function(err) {//changes the name of template.scrivx file to the final name
-    if ( err ) console.log('ERROR: ' + err);
-});
+function main(){
+  var excel = xlsx.parse(f); // parses the excel file
+  var text = fs.readFileSync('./template.scriv/template.scrivx').toString('utf-8'); //reads the template.scrivx file and puts it in text
+  fs.unlinkSync(scriv + '/template.scrivx');//deletes the template.scrivx file from the destination
+  var keywords = getKeywords(excel);
+  text = text.replace('<Keywords></Keywords>', buildKeywords(excel,keywords));
+  buildMap(excel,keywords);
+  text = text.replace('<CustomMetaDataSettings></CustomMetaDataSettings>' , buildCustomMetaDataSettings(excel)); //builds CustomMetaDataSettings part and puts it in the text
+  fs.writeFile(scrivx, text, function(err) {//writes the text to scrivx file
+      if(err) {
+          return console.log(err);
+      }
+      console.log("The file was saved!");
+  });
 
+}
 
-
-
-
-
-
-var excel = xlsx.parse(f); // parses the excel file
-var text = fs.readFileSync('./template.scriv/template.scrivx').toString('utf-8'); //reads the text from scrivx file
-var keywords = getKeywords(excel);
-buildKeywords(excel,keywords);
-text = text.replace('<CustomMetaDataSettings></CustomMetaDataSettings>' , buildCustomMetaDataSettings(excel)); //builds CustomMetaDataSettings part and puts it in the text
-text = text.replace('<Keywords></Keywords>', buildKeywords(excel,keywords));
-fs.writeFile(scrivx, text, function(err) {//writes the text to scrivx file
-    if(err) {
-        return console.log(err);
-    }
-
-    console.log("The file was saved!");
-});
 
 
 function buildCustomMetaDataSettings(excel){
@@ -75,10 +71,29 @@ function getKeywords (excel){ //makes an object of all the classes and returns t
 function buildKeywords(excel,keywords){
   var str = '';
   for (var key in keywords) {
-    str += '<Keyword ID="' + keywords[key] + '">\n<Title>' + key + '</Title>\n<Color>0.614707 0.655123 1.0</Color></Keyword>'
+    str += '     <Keyword ID="' + keywords[key] + '">\n        <Title>' + key + '</Title>\n       <Color>0.614707 0.655123 1.0</Color></Keyword>\n'
   }
+  str = '<Keywords>\n' + str + '    </Keywords>'
   return str
 }
+
+function buildMap(excel,keywords){
+  var mapStr = '';
+  var rows = excel[0].data;
+  rows.forEach(function(row,index){
+    buildBinderItem(row,rows,index);
+  })
+}
+
+function buildBinderItem(row,rows,index){
+  var binderItem = '<BinderItem UUID="' + index;
+  // fs.mkdirSync(scriv+'/files/data/' + index);
+}
+
+
+
+
+
 
 
 
